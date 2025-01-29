dff0fc2a2ad583fc6890a962827b5e4f7795f8c5
domiuau <domiuaunoob@outlook.com>
2025-01-29 14:47:59 -0300

    Initial Commit

---
+++ TestesParaChallenge4/TestesParaChallenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  TestesParaChallenge4App.swift
+//  TestesParaChallenge4
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TestesParaChallenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
